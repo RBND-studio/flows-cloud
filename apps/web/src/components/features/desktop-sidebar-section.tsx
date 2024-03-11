e2d0b02bf79d306bb86f1a091468c10b650ef407
Vojtěch Vidra <vojtechvidra@gmail.com>
2024-03-11 10:53:55 +0100

    fix

---
+++ apps/web/src/components/features/desktop-sidebar-section.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { css } from "@flows/styled-system/css";
+import { Flex } from "@flows/styled-system/jsx";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import type { FC, ReactElement } from "react";
+
+type Props = {
+  title: string;
+  link: string;
+  items?: ReactElement[];
+};
+
+export const DesktopSidebarSection: FC<Props> = ({ link, title, items }) => {
+  const pathname = usePathname();
+  const active = pathname.startsWith(link);
+
+  return (
+    <>
+      <Link
+        className={css({
+          mb: "space16",
+          textStyle: "titleM",
+          color: active ? "text.primary" : "text",
+          _hover: { textDecoration: "underline" },
+        })}
+        color={pathname.startsWith(link) ? "primary" : undefined}
+        href={link}
+      >
+        {title}
+      </Link>
+      <Flex flexDirection="column" gap="space8" mb="space24" srOnly={!active}>
+        {items}
+      </Flex>
+    </>
+  );
+};
