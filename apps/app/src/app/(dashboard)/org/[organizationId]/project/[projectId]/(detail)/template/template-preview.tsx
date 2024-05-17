00fd642846e4dd423c4588571bd89361a832a8d0
Vojtěch Vidra <vojtechvidra@gmail.com>
2024-05-17 09:04:34 +0200

    feat(app): tooltip, modal side by side template preview
    closes #96

--- apps/app/src/app/(dashboard)/org/[organizationId]/project/[projectId]/(detail)/template/template-preview.tsx
+++ apps/app/src/app/(dashboard)/org/[organizationId]/project/[projectId]/(detail)/template/template-preview.tsx
@@ -1,11 +1,10 @@
 "use client";
 
-import { endFlow, startFlow } from "@flows/js";
-import { init } from "@flows/js/core";
-import { Box, Flex } from "@flows/styled-system/jsx";
+import { renderModalElement, renderTooltipElement, updateTooltip } from "@flows/js/core";
+import { Box, Flex, Grid } from "@flows/styled-system/jsx";
 import { useFirstRender } from "hooks/use-first-render";
 import type { FC } from "react";
-import { useEffect, useMemo } from "react";
+import { useEffect, useMemo, useRef } from "react";
 
 import { useTemplate } from "./template-context";
 
@@ -14,48 +13,74 @@ export const TemplatePreview: FC = () => {
   const { cssTemplate, cssVars } = useTemplate();
   const cssStyle = useMemo(() => [cssVars, cssTemplate].join(""), [cssTemplate, cssVars]);
 
+  const tooltipRootRef = useRef<HTMLDivElement>(null);
+  const tooltipTargetRef = useRef<HTMLDivElement>(null);
+  const modalRootRef = useRef<HTMLDivElement>(null);
+
   useEffect(() => {
     if (firstRender) return;
-    void init({
-      rootElement: "#template-preview-root",
-      flows: [
-        {
-          id: "flow",
-          steps: [
-            {
-              title: "Tooltip",
-              body: "Hello world!",
-              targetElement: ".tooltip-target",
-              nextLabel: "Show modal",
-            },
-            {
-              title: "Modal",
-              body: "This is a modal!",
-            },
-          ],
-        },
-      ],
-    });
-    startFlow("flow");
+    if (!tooltipRootRef.current || !tooltipTargetRef.current || !modalRootRef.current) return;
 
-    return () => endFlow("flow");
+    const tooltip = renderTooltipElement({
+      root: tooltipRootRef.current,
+      target: tooltipTargetRef.current,
+      step: {
+        title: "Tooltip",
+        // cspell:disable-next-line
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        targetElement: ".tooltip-target",
+      },
+      isFirstStep: false,
+      isLastStep: false,
+    });
+    void updateTooltip({
+      target: tooltipTargetRef.current,
+      tooltip: tooltip.tooltip,
+      arrowEls: tooltip.arrows,
+      boundary: tooltip.root,
+    });
+
+    const modal = renderModalElement({
+      root: modalRootRef.current,
+      isFirstStep: false,
+      isLastStep: false,
+      step: {
+        title: "Modal",
+        // cspell:disable-next-line
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      },
+    });
+
+    return () => {
+      tooltip.tooltip.remove();
+      modal.root.querySelector("flows-modal")?.remove();
+    };
   }, [firstRender]);
 
   if (firstRender) return null;
 
   return (
     <>
-      <Flex
-        alignItems="center"
-        cardWrap="-"
-        h="200px"
-        id="template-preview-root"
-        overflow="hidden"
-        p="space40"
-        transform="translate3d(0,0,0)"
-      >
-        <Box bg="bg.primary" className="tooltip-target" h="16px" rounded="radius100" w="16px" />
-      </Flex>
+      <Grid gridTemplateColumns="1fr 1fr" gap="space16" h="200px">
+        <Flex
+          ref={tooltipRootRef}
+          alignItems="center"
+          cardWrap="-"
+          overflow="hidden"
+          p="space40"
+          transform="translate3d(0,0,0)"
+        >
+          <Box
+            bg="bg.primary"
+            ref={tooltipTargetRef}
+            className="tooltip-target"
+            h="16px"
+            rounded="radius100"
+            w="16px"
+          />
+        </Flex>
+        <Box ref={modalRootRef} cardWrap="-" transform="translate3d(0,0,0)" overflow="hidden" />
+      </Grid>
       <style>{cssStyle}</style>
     </>
   );
