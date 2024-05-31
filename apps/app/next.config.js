5b47db7332b468f7ea444130f775405638557138
Vojtěch Vidra <vojtechvidra@gmail.com>
2024-05-31 10:07:42 +0200

    fix(app): csp for plausible (#366)

--- apps/app/next.config.js
+++ apps/app/next.config.js
@@ -5,7 +5,7 @@ const dev = process.env.NODE_ENV !== "production";
 
 const cspHeader = `
     default-src 'self';
-    connect-src 'self' https://*.flows.sh https://*.flows-cloud.com https://*.posthog.com${
+    connect-src 'self' https://flows.sh https://*.flows.sh https://*.flows-cloud.com https://*.posthog.com${
       dev ? " http://127.0.0.1:3005 http://localhost:3005" : ""
     };
     script-src 'self' 'unsafe-eval' 'unsafe-inline' https://flows.sh https://*.lemonsqueezy.com https://challenges.cloudflare.com https://*.posthog.com https://cdn.jsdelivr.net;
