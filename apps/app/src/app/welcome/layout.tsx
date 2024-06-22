0501a79f1a07c22b10565230a6fdf73ca80004ec
Ondřej Pešička <77627332+OPesicka@users.noreply.github.com>
2024-06-22 09:32:54 +0200

    feat(app,web,docs): new homepage and branding (#381)
    * feat(app,web,docs): new homepage and branding
    
    * update logos
    
    * feat(web): demo tabs
    
    * illustrations
    
    * feature illustrations
    
    * favicons
    
    * hero illustrations
    
    * og images
    
    * readme
    
    * small tweaks
    
    * darkmode demo
    
    * feat: remove Mona-Sans, fix hero image
    
    * details
    
    * update palette and link to dark in atmos
    
    * fix spacing
    
    ---------
    
    Co-authored-by: Vojtěch Vidra <vojtechvidra@gmail.com>

--- apps/app/src/app/welcome/layout.tsx
+++ apps/app/src/app/welcome/layout.tsx
@@ -1,10 +1,9 @@
 import { Flex, Grid } from "@flows/styled-system/jsx";
 import { getAuth } from "auth/server";
-import Image from "next/image";
 import { redirect } from "next/navigation";
 import type { ReactNode } from "react";
 import { routes } from "routes";
-import { Text } from "ui";
+import { Logo } from "ui";
 
 import { TopRightButtons } from "./top-right-buttons";
 import { WelcomeProviders } from "./welcome-providers";
@@ -24,10 +23,7 @@ export default async function WelcomeLayout({ children }: Props): Promise<JSX.El
           {children}
         </Flex>
         <Flex alignItems="center" gap="space8" left="space24" position="absolute" top="space24">
-          <Image alt="Logo" height={28} priority src="/logo.svg" width={28} />
-          <Text variant="bodyM" weight="700">
-            Flows
-          </Text>
+          <Logo type="type" size={24} />
         </Flex>
         <TopRightButtons />
       </Grid>
