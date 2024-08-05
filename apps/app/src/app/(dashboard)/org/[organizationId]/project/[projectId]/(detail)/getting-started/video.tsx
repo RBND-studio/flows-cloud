723d1ce381015201ea705d45024db8ffeffae874
Vojtěch Vidra <vojtechvidra@gmail.com>
2024-08-05 13:17:04 +0200

    refactor video to dialog

--- apps/app/src/app/(dashboard)/org/[organizationId]/project/[projectId]/(detail)/getting-started/video.tsx
+++ apps/app/src/app/(dashboard)/org/[organizationId]/project/[projectId]/(detail)/getting-started/video.tsx
@@ -1,69 +1,17 @@
 "use client";
 
 import { css } from "@flows/styled-system/css";
-import { Box, Flex } from "@flows/styled-system/jsx";
+import { Box } from "@flows/styled-system/jsx";
 import { Play40 } from "icons";
 import Image from "next/image";
-import { useEffect, useState } from "react";
-import { Icon } from "ui";
+import { useState } from "react";
+import { Dialog, Icon } from "ui";
 
 import demoVideoCover from "./demo-video-cover.png";
 
-const VideoOverlay = ({ hide }: { hide: () => void }): JSX.Element => {
-  useEffect(() => {
-    const handleKeyDown = (event: KeyboardEvent): void => {
-      if (event.key === "Escape") {
-        hide();
-      }
-    };
-
-    window.addEventListener("keydown", handleKeyDown);
-
-    return () => {
-      window.removeEventListener("keydown", handleKeyDown);
-    };
-  }, [hide]);
-
-  return (
-    <Box
-      position="absolute"
-      top={0}
-      left={0}
-      right={0}
-      bottom={0}
-      display="flex"
-      alignItems="center"
-      justifyContent="center"
-      bg="rgba(0, 0, 0, 0.5)"
-      color="white"
-      onClick={hide}
-      zIndex="1000"
-    >
-      <Flex maxWidth="1200px" p="space24" maxHeight="675px" width="100%" height="100%">
-        <Box position="relative" width="100%" height="0" pb="56.25%">
-          <iframe
-            src="https://www.youtube.com/embed/XY3eU_p0AW4?si=kSQJFruZM8YViA0w"
-            title="YouTube video player"
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            referrerPolicy="strict-origin-when-cross-origin"
-            allowFullScreen
-            className={css({
-              position: "absolute",
-              top: 0,
-              left: 0,
-              width: "100%",
-              height: "100%",
-            })}
-          />
-        </Box>
-      </Flex>
-    </Box>
-  );
-};
-
 export const VideoSection = (): JSX.Element => {
   const [showVideo, setShowVideo] = useState(false);
+
   return (
     <>
       <Box
@@ -98,7 +46,25 @@ export const VideoSection = (): JSX.Element => {
         />
         <Image alt="Flows demo video cover" height={1624} src={demoVideoCover} width={2880} />
       </Box>
-      {showVideo ? <VideoOverlay hide={() => setShowVideo(false)} /> : null}
+      <Dialog open={showVideo} onOpenChange={setShowVideo} maxWidth={1200}>
+        <Box position="relative" width="100%" height="0" pb="56.25%">
+          <iframe
+            src="https://www.youtube.com/embed/XY3eU_p0AW4?si=kSQJFruZM8YViA0w"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerPolicy="strict-origin-when-cross-origin"
+            allowFullScreen
+            className={css({
+              position: "absolute",
+              top: 0,
+              left: 0,
+              width: "100%",
+              height: "100%",
+            })}
+          />
+        </Box>
+      </Dialog>
     </>
   );
 };
