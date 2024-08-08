540252aad635ff7c4043509bf334b3678c192c1f
Vojtěch Vidra <vojtechvidra@gmail.com>
2024-08-08 13:32:03 +0200

    fix(be): sdk null origin request (#491)

--- apps/backend/src/lib/origin.ts
+++ apps/backend/src/lib/origin.ts
@@ -1,8 +1,18 @@
-export const isLocalhost = (origin: string): boolean => {
-  const url = new URL(origin);
-  const originWithoutPort = `${url.protocol}//${url.hostname}`;
+import { sanitize } from "./sanitize";
 
-  return ["http://localhost", "https://localhost", "http://127.0.0.1", "https://127.0.0.1"].some(
-    (o) => o === originWithoutPort,
-  );
+export const isLocalhost = (origin: string): boolean => {
+  if (origin === "null") return false;
+
+  try {
+    const url = new URL(origin);
+    const originWithoutPort = `${url.protocol}//${url.hostname}`;
+
+    return ["http://localhost", "https://localhost", "http://127.0.0.1", "https://127.0.0.1"].some(
+      (o) => o === originWithoutPort,
+    );
+  } catch (e) {
+    // eslint-disable-next-line no-console -- helpful for debugging
+    console.error("Error parsing origin", sanitize(origin), e);
+    return false;
+  }
 };
---
+++ apps/backend/src/lib/sanitize.ts
@@ -0,0 +1,1 @@
+export const sanitize = (str: string): string => str.replace(/\n|\r/g, "");
