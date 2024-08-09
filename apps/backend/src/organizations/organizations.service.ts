dc4277d87cfba3fd2a11471b1ca0c642b2827a21
Vojtěch Vidra <vojtechvidra@gmail.com>
2024-08-09 08:58:28 +0200

    fix(be): get organizations

--- apps/backend/src/organizations/organizations.service.ts
+++ apps/backend/src/organizations/organizations.service.ts
@@ -47,9 +47,9 @@ export class OrganizationsService {
   async getOrganizations({ auth }: { auth: Auth }): Promise<GetOrganizationListItemDto[]> {
     const otu = alias(organizationsToUsers, "otu");
     const otuQuery =
-      sql<number>`SELECT COUNT(${otu.user_id}) FROM organization_to_user otu WHERE ${otu.organization_id} = ${organizations.id}`
-        .mapWith(Number)
-        .as("members_count");
+      sql<number>`(SELECT COUNT(${otu.user_id}) FROM organization_to_user otu WHERE ${otu.organization_id} = ${organizations.id}) as members_count`.mapWith(
+        Number,
+      );
     const query = this.databaseService.db
       .select({
         organization: organizations,
